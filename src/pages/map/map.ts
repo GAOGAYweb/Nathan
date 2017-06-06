08d0fac5174ade0148a7872e93e03dac3208de5c
wzy <you@example.com>
2017-06-06 14:43:13 +0800

    map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -59,30 +59,26 @@ export class MapPage{
         this.moments.push(new_moment);
       }
 
-      let map = this.map = new BMap.Map(this.mapElement.nativeElement, {enableMapClick: true});//创建地图实例
+      let map = new BMap.Map(this.mapElement.nativeElement, {enableMapClick: true});//创建地图实例
       map.enableScrollWheelZoom();//启动滚轮放大缩小，默认禁用
       map.enableContinuousZoom();//连续缩放效果，默认禁用
       let point = new BMap.Point(121.604,31.196);//坐标可以通过百度地图坐标拾取器获取
       map.centerAndZoom(point, 18);//设置中心和地图显示级别
 
-      var mkr,ptr;
-      var myIcon = new BMap.Icon("assets/img/markers.png", new BMap.Size(23, 25));
+      let mkr, ptr;
+      const myIcon = new BMap.Icon("assets/img/markers.png", new BMap.Size(23, 25));
       for(let i = 0;i < this.moments.length; i++){
         let moment = this.moments[i];
+        console.log("map moment", moment.x, moment.y);
         ptr = new BMap.Point(moment.x, moment.y);
         mkr = new BMap.Marker(ptr,{icon:myIcon});
         mkr.addEventListener('click', ()=>{
-          console.log(moment);
           let modal = this.modalCtrl.create(ModalMomentDetailPage, {moment:moment});
           modal.present();
-          // mkr.openInfoWindow(new BMap.InfoWindow(moment.content, {
-          //   width : 250,
-          //   height: 100,
-          //   title : moment.author
-          // }    ));
         });
         mkr.setLabel(new BMap.Label(this.moments[i].content,{offset:new BMap.Size(20,-10)}));
         map.addOverlay(mkr);
+        this.map = map;
       }
     })
   }
